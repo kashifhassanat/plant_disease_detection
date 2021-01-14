e58e965a126d6fc1d0173bd0e35102c591d4058c
SyedMuhammadRehan <syedrehan55.rs@gmail.com>
2021-01-14 15:08:55 +0500

    detectimage

---
+++ lib/app/screens/detectimage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetectImage extends StatefulWidget {
+  @override
+  _DetectImageState createState() => _DetectImageState();
+}
+
+class _DetectImageState extends State<DetectImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
